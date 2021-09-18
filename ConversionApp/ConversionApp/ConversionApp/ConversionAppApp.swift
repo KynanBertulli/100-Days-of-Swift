b88a85d94869ab07f8a2e3717565899a237610a8
Kynan Bertulli <Kynanbertulli@gmail.com>
2021-09-17 22:40:43 -0500

    this is adding my first iOS app

---
+++ ConversionApp/ConversionApp/ConversionApp/ConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionAppApp.swift
+//  ConversionApp
+//
+//  Created by Kynan Bertulli on 2021-09-17.
+//
+
+import SwiftUI
+
+@main
+struct ConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
